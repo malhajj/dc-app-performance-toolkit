b6e7952799c4ef7268f0a2628c94fdd9886f2922
Ahmad Alaiti <aalaiti1993@gmail.com>
2023-02-25 18:03:35 +0200

    adjustments to tests

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -33,31 +33,3 @@ def app_specific_action_js_mobile_service_management_portal_test1(webdriver, dat
         page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/NotificationsConfigWebActionSupport.jspa")
         page.wait_until_visible((By.ID, "mfjsdcp-admin-link"))
     measure()
-
-def app_specific_action_js_mobile_service_management_portal_test2(webdriver, datasets):
-    page = BasePage(webdriver)
-
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    @print_timing("selenium_app_specific_user_login")
-    def measure():
-        def app_specific_user_login(username='admin', password='admin'):
-            login_page = Login(webdriver)
-            login_page.delete_all_cookies()
-            login_page.go_to()
-            login_page.set_credentials(username=username, password=password)
-            if login_page.is_first_login():
-                login_page.first_login_setup()
-            if login_page.is_first_login_second_page():
-                login_page.first_login_second_page_setup()
-            login_page.wait_for_page_loaded()
-        app_specific_user_login(username='admin', password='admin')
-    measure()
-
-    @print_timing("app_specific_action_js_mobile_service_management_portal_test2")
-    def measure():
-        page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/NotificationsConfigWebActionSupport.jspa")
-        page.wait_until_visible((By.ID, "project-filter"))
-    measure()
