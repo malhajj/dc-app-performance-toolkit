e1e6ae61171009e6470e2d3368b06748c6a25999
Ahmad Alaiti <aalaiti1993@gmail.com>
2023-04-04 08:55:34 +0300

    added needed tests

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -30,34 +30,6 @@ def test_1_selenium_custom_action_test1(webdriver, datasets):
 
     @print_timing("test_1_selenium_custom_action_test1")
     def measure():
-        page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/LogLevel.jspa")
-        page.wait_until_visible((By.ID, "tcfj-log-level-admin-link"))
-    measure()
-
-def test_2_selenium_custom_action_test2(webdriver, datasets):
-    page = BasePage(webdriver)
-
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    @print_timing("selenium_app_specific_user_login")
-    def measure():
-        def app_specific_user_login(username='admin', password='admin'):
-            login_page = Login(webdriver)
-            login_page.delete_all_cookies()
-            login_page.go_to()
-            login_page.set_credentials(username=username, password=password)
-            if login_page.is_first_login():
-                login_page.first_login_setup()
-            if login_page.is_first_login_second_page():
-                login_page.first_login_second_page_setup()
-            login_page.wait_for_page_loaded()
-        app_specific_user_login(username='admin', password='admin')
-    measure()
-
-    @print_timing("test_2_selenium_custom_action_test2")
-    def measure():
-        page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/TwitterAdminConfig.jspa")
-        page.wait_until_visible((By.ID, "tcfj-admin-config-link"))
+        page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/BlockUsersWebActionSupport.jspa")
+        page.wait_until_visible((By.ID, "block-users-admin-link"))
     measure()
